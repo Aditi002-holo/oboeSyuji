74a89b95d461dffa391bf82e739abaf20d9e3215
Aditi Deshpande <aditideshpande2908@gmail.com>
2023-09-14 10:11:22 +0530

    Refactor the code

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,24 @@
-// how many numbers can you recite?
 const subTitieEl = document.querySelector('.sub-title')
 const rangeBtnEl = document.querySelectorAll('.range-btn')
 
+// Event Listeners
 rangeBtnEl.forEach(function(rangeBtn) {
     rangeBtn.addEventListener('click',function() {
-        subTitieEl.textContent = 'how many numbers can you recite?'  
+        updateSubTitleContent()
+
         let initialValue = rangeBtn.dataset.value
-        console.log(initialValue)
-
-        document.querySelector('.btn-wrapper').style.display = "none"
-
-        document.querySelector('.recite-content').style.display = "initial"
-        document.querySelector('.recite-number').innerHTML = initialValue
+        updateInitialValue(initialValue)
+        
     })
 })
-// rangeBtnEl.addEventListener('click', function() {
-//     subTitieEl.textContent = 'how many numbers can you recite?'
-//     console.log('change success')
-// })
 
+// Functions
+function updateSubTitleContent() {
+    subTitieEl.textContent = 'how many numbers can you recite?' 
+}
+
+function updateInitialValue(initialValue) {
+    document.querySelector('.btn-wrapper').style.display = "none"
+    document.querySelector('.recite-content').style.display = "initial"
+    document.querySelector('.recite-number').innerHTML = initialValue
+}
