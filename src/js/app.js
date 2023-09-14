b57f21292da1666057033af20b8898074f812f3a
Aditi Deshpande <aditideshpande2908@gmail.com>
2023-09-14 10:37:12 +0530

    Add random numbers for a range logic

--- src/js/app.js
+++ src/js/app.js
@@ -6,8 +6,9 @@ rangeBtnEl.forEach(function(rangeBtn) {
     rangeBtn.addEventListener('click',function() {
         updateSubTitleContent()
 
-        let initialValue = rangeBtn.dataset.value
-        updateInitialValue(initialValue)
+        let lowerBoundValue = rangeBtn.dataset.lower
+        let upperBoundValue = rangeBtn.dataset.upper
+        updateInitialValue(lowerBoundValue, upperBoundValue)
         
     })
 })
@@ -17,8 +18,16 @@ function updateSubTitleContent() {
     subTitieEl.textContent = 'how many numbers can you recite?' 
 }
 
-function updateInitialValue(initialValue) {
+function updateInitialValue(lowerBoundValue, upperBoundValue) {
     document.querySelector('.btn-wrapper').style.display = "none"
     document.querySelector('.recite-content').style.display = "initial"
-    document.querySelector('.recite-number').innerHTML = initialValue
+
+    const reciteNumberEl = document.querySelector('.recite-number')
+    reciteNumberEl.textContent = lowerBoundValue
+
+    reciteNumberEl.addEventListener('click', function() {
+        let difference = (upperBoundValue - lowerBoundValue) + 1
+        let randomNumbers = Math.floor(Math.random() * difference) + Number(lowerBoundValue)
+        reciteNumberEl.textContent = randomNumbers
+    })
 }
